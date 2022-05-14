78bdf4c7787c13717a6a5281937198ef7b356ebf
William <williamalvarez672@gmail.com>
2022-05-14 14:25:10 +0100

    setting up

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import logo from './avatar1.png';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcom to Mikita's Page!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
