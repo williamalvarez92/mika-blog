a92ff371a9f36633650ff8c604202d9d4a3f3b6c
William <williamalvarez672@gmail.com>
2022-05-14 14:55:32 +0100

    typos and bg

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcom to Mikita's Page!</h1>
+        <h1>Welcome to Mikita's Page!</h1>
       </header>
     </div>
   );
