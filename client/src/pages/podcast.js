05e0f44613d4966b86b5c148884a496b7076b3dc
William <williamalvarez672@gmail.com>
2022-05-25 21:49:30 +0100

    ome work

--- client/src/pages/podcast.js
+++ client/src/pages/podcast.js
@@ -1,10 +1,18 @@
+import avatar from '../avatar1.png';
 
 function PodCast() {
 
   return (
 
     <div className="mainpage">
-        <h1 style={{color: 'white'}}>The PodCast info will come here</h1>
+    <div className='bubble-container'>
+        <img src={avatar} className="avatar-logo" alt="logo" />
+        <div className="box bubble"> 
+    <div className='contact'>
+    <h5>This is the section where I talk about my podcast!!!!</h5>
+    </div>
+        </div>
+    </div>
     </div>
   )
 }
