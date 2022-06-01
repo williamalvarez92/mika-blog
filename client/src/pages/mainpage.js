6dfe7cda35f919cdbe8749eb35bcfdbd2f56ee68
William <williamalvarez672@gmail.com>
2022-06-01 17:33:29 +0100

    adding some node

--- client/src/pages/mainpage.js
+++ client/src/pages/mainpage.js
@@ -1,8 +1,26 @@
 import avatar from '../avatar1.png';
 import '../styles/mainpage.scss';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { getTokenFromLocalStorage } from '../helpers/auth';
 
 function MainPage() {
+const [ allArticles, setArticles ] = useState([])
 
+useEffect(() => {
+    const getData = async () => {
+        try {
+        const { data } = await axios.get(
+            'http://localhost:4000/api/allarticles'
+        )
+            setArticles(data)
+        } catch (error) {
+        console.log(error)
+        }
+    }
+    getData()
+    }, [])
+console.log(allArticles)
 
 
 return (
@@ -13,6 +31,7 @@ return (
         <div class="type-writer-text">
         会計士 in ロンドンです。 私のブログへようこそ!
         </div>
+
     </div>
     </div>
 </div>
